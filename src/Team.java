4bbb9c8053301bc8920f03a79063cf6f88cd12a6
Camotubi <camotubi@gmail.com>
2017-06-12 08:31:59 -0500

    add meb folder y Team files

--- src/Team.java
+++ src/Team.java
@@ -1,4 +1,27 @@
 
 public class Team {
+	private int cod_equipo;
+	private String descripccion;
+	private int tipo;
+	public int getCod_equipo() {
+		return cod_equipo;
+	}
+	public void setCod_equipo(int cod_equipo) {
+		this.cod_equipo = cod_equipo;
+	}
+	public String getDescripccion() {
+		return descripccion;
+	}
+	public void setDescripccion(String descripccion) {
+		this.descripccion = descripccion;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 
 }
